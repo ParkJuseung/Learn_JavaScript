a1fcc966c97aaffc42ff1b71a7f018da3ae401f0
PJS <judang0322@gmail.com>
2022-03-23 23:44:09 +0900

    03.23 javascript

--- Chrome_App/app.js
+++ Chrome_App/app.js
@@ -1,8 +1,17 @@
 function getLastApp(apps) {
-    let count = apps.lentgh - 1
-    return apps[count]
+    return console.log(apps[apps.length-1])
 }
 
 // Sample usage - do not modify
 console.log(getLastApp(["Chrome", "Clock", "Twitter"])); // "Twitter"
 console.log(getLastApp(["Safari", "Contacts"])); // "Contacts"
+
+
+function loopThroughElements(elements) {
+    elements.forEach(function(element) {
+        return console.log(element)
+    })
+}
+
+// Sample usage - do not modify
+loopThroughElements(["Sam", "Charlie", "Alex"]); // should log every name to the console
